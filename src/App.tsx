64f30e5ef2e367e01c4f9f74de3212cdc89d69cb
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-12-20 01:04:20 +0100

    refactor: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-	let c;
-
 	return (
 		<div className="App">
 			<h1>Anonime</h1>
