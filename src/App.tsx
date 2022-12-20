ee991fec92e58da7d62f7b9058a14af3ccae0480
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-12-20 01:20:20 +0100

    refactor: clean up default vite project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
-function App() {
-	return (
-		<div className="App">
-			<h1>Anonime</h1>
-		</div>
-	);
-}
+const App = () => {
+	return <h1 className="text-3xl font-bold underline">Anonime</h1>;
+};
 
 export default App;
