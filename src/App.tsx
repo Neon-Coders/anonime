0335371d88331b7907c6fcac7a6771548bbf1d4a
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-12-25 14:53:38 +0100

    style: homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Anonime</h1>;
+	return (
+		<main className="text-[1.5rem] text-gray-600 flex flex-col items-center justify-center text-center h-screen">
+			<h1 className="text-[9vw] m-0 p-0 font-extrabold hover:underline">
+				<a
+					href="https://github.com/ibrahimraimi/anonime"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Anonime
+				</a>
+			</h1>
+			<p className="text-[1.3rem] text-gray-400">Anime and Manga collection</p>
+		</main>
+	);
 };
 
 export default App;
