863063feb055acb8a4f412853cbcc8ad888323a9
Ibrahim Raimi <raimiibrahim44@gmail.com>
2023-01-28 22:10:34 +0100

    feat: add manga page

--- src/pages/Manga.tsx
+++ src/pages/Manga.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
+import { TopManga } from "../data";
 
 const Manga: FC = () => {
-	return <div>Manga</div>;
+	return (
+		<div className="mt-[3rem]">
+			<p className="text-gray-500 text-base pt-3 mb-5 md:text-lg">
+				What are you gonna read today?
+			</p>
+
+			<main>
+				<TopManga />
+			</main>
+		</div>
+	);
 };
 
 export default Manga;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 import { Search } from "../components";
-import { TopAnime } from "../data";
+import { TopAnime, RandomAnime, TopManga } from "../data";
 import Home from "./Home";
 import Manga from "./Manga";
 import SearchAnime from "./SearchAnime";
 
-export { Home, Manga, Search, TopAnime, SearchAnime };
+export { Home, Manga, Search, TopAnime, RandomAnime, TopManga, SearchAnime };
