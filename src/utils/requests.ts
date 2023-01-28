7e4dba339a7380b7b0e2fb144ac0cbb693683a9e
Ibrahim Raimi <raimiibrahim44@gmail.com>
2023-01-28 22:09:33 +0100

    chore: add manga url

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = 'https://api.jikan.moe/v4/anime';
-export const FETCH_TOP_ANIME = 'https://api.jikan.moe/v4/top/anime';
-export const FETCH_RANDOM_ANIME = 'https://api.jikan.moe/v4/random/anime';
+export const BASE_URL = "https://api.jikan.moe/v4/anime";
+export const MANGA_BASE_URL = "https://api.jikan.moe/v4/manga";
+export const FETCH_TOP_ANIME = "https://api.jikan.moe/v4/top/anime";
+export const FETCH_RANDOM_ANIME = "https://api.jikan.moe/v4/random/anime";
