f72ecbe6045663d8cb31124b66971927b3405f12
Ibrahim Raimi <raimiibrahim44@gmail.com>
2023-01-28 22:05:15 +0100

    feat: random anime

---
+++ src/data/RandomAnime.tsx
@@ -0,0 +1,46 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { FC } from "react";
+import useSWR, { Fetcher } from "swr";
+import { BASE_URL } from "../utils";
+import { Loader, ServerError } from "../components";
+
+const fetcher = (...args: Fetcher[]) =>
+	fetch(...args).then((response) => response.json());
+
+const RandomAnime: FC = () => {
+	const { data, isLoading, error } = useSWR(BASE_URL, fetcher, {
+		suspense: true,
+	});
+
+	const randomAnime: [] = data.data;
+
+	if (isLoading) return <Loader />;
+	if (error) return <ServerError />;
+
+	console.log(randomAnime);
+
+	return (
+		<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-4 gap-y-5">
+			{randomAnime.map((anime) => {
+				return (
+					<div
+						key={anime.mal_id}
+						className="flex flex-col items-center group h-[374px] overflow-hidden"
+					>
+						<div className="overflow-hidden rounded-xl grid relative cursor-pointer">
+							<img
+								src={anime.images.webp.large_image_url}
+								className="object-cover w-full h-[310px] group-hover:scale-105 transition-all duration-300"
+								alt=""
+							/>
+						</div>
+						<h5 className="anime_title">{anime.title}</h5>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+export default RandomAnime;
